dfe5d3ca8fdd8431a3331fc96be96445925be5fa
xguenard <xavier.guenard@gmail.com>
2016-06-07 00:03:10 +0200

    more on capture engine

--- capture_engine.py
+++ capture_engine.py
@@ -1,7 +1,38 @@
 #Image capture engine
 
 import pyscreenshot as ImageGrab
+import time
+
+class CaptureEngine:
+    """
+        Class responsible of taking picture of the screen every two seconds
+    """
+
+    def __init__(self):
+       self.delay = 2 # time between screen shots
+       self.names = ("screenshot1", "screenshot2", "screenshot3")
+
+
+    def start(self, nb_iter):
+        """
+            Launch the scan loop
+        """
+        round = 0
+        counter = 0
+        while round < nb_iter:
+            print("Saving screenshot in "+self.names[counter] )
+            im = ImageGrab.grab_to_file( self.names[counter] )
+            print("Screenshot done")
+            time.sleep(self.delay)
+            ++round
+            counter = (counter+1)%3
+
+
+
+
 
 if __name__ == "__main__":
-    im = ImageGrab.grab()
-    im.show()
+    test_engine = CaptureEngine()
+    test_engine.start(20)
+
+
